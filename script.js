8d3640d79c1cd5202e2bfe1a93cc8cdc7f8547ac
Apexshya <gyawaliapeksha123@gmail.com>
2023-10-31 23:57:38 +0545

    new changes

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
-let y = Math.floor(Math.random() * 10 + 1);
-let guess = 1;
+document.addEventListener("DOMContentLoaded", function () {
+    let y = Math.floor(Math.random() * 100) + 1; // Generate a random number between 1 and 100
+    let guess = 1;
+    const maxGuesses = 10; // Set the maximum number of guesses
 
-document.getElementById("submitGuess").onclick = function () {
-    let x = parseInt(document.getElementById("guessField").value);
+    document.getElementById("submitGuess").addEventListener("click", function () {
+        let x = parseInt(document.getElementById("guessField").value);
+        let messageElement = document.getElementById("message");
 
-    if (x == y) {
-        alert("CONGRATULATIONS!!! YOU GUESSED IT RIGHT IN " + guess + " GUESSES");
-    } else if (x > y) {
-        guess++;
-        alert("OOPS SORRY!! TRY A SMALLER NUMBER");
-    } else {
-        guess++;
-        alert("OOPS SORRY!! TRY A GREATER NUMBER");
-    }
-};
+        if (guess <= maxGuesses) {
+            if (x == y) {
+                messageElement.textContent = "CONGRATULATIONS!!! YOU GUESSED IT RIGHT IN " + guess + " GUESSES";
+            } else if (x > y) {
+                messageElement.textContent = "OOPS SORRY!! TRY A SMALLER NUMBER";
+            } else {
+                messageElement.textContent = "OOPS SORRY!! TRY A GREATER NUMBER";
+            }
+
+            guess++;
+
+            if (guess > maxGuesses) {
+                document.getElementById("submitGuess").disabled = true;
+                messageElement.textContent = "Sorry, you've reached the maximum number of guesses. The correct number was " + y;
+            }
+        }
+    });
+});
